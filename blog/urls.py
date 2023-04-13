a3731119b9cb1540ef8536325e33db241d7c21ea
Cristian Machuca <cmachuca32@gmail.com>
2023-04-13 10:37:15 +0000

    Updating Sass.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from blog import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('register', views.register, name="register"),
+    path('login', views.login, name="login"),
+    path('contact', views.contact, name="contact"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,24 @@ from django.http import HttpResponse
 
 def index(request):
     params= {}
-    params['site_name'] = "@Django Blog"
+    params['site_name'] = '@Django Blog-home'
 
     return render(request, 'blog/index.html', params)
+
+def register(request):
+    params= {}
+    params['site_name'] = '@Django Blog-register'
+
+    return render(request, 'blog/register.html', params)
+
+def login(request):
+    params= {}
+    params['site_name'] = '@Django Blog-login'
+    
+    return render(request, "blog/login.html", params)
+
+def contact(request):
+    params= {}
+    params['site_name'] = '@Django Blog-contact'
+
+    return render(request, 'blog/contact.html', params)
